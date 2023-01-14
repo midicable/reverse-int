4baaa5119695237046f123d557cca991b6734443
midicable <bogdan.astronomy@gmail.com>
2023-01-14 21:06:17 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+  n = Math.abs(n);
+  let reversedNumber = '';
+
+  while (n > 0) {
+    reversedNumber += n % 10;
+    n = Math.floor(n / 10);
+  }
   
+  return reversedNumber;
 }
